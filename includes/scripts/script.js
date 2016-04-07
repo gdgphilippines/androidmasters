1e3c281b09103d95d666c20b35b98090526b0c2a
Juvar Abrera <me@juvarabrera.com>
2016-04-07 19:18:43 +0800

    Added slider

--- includes/scripts/script.js
+++ includes/scripts/script.js
@@ -1,13 +1,19 @@
+var logo, label, nav;
 function responsive() {
 	$parent = $(".action-bar");
-	console.log($parent.children("#logo").width() + $parent.children("ul").width());
-	console.log($parent.width() - $parent.children(".register").width());
-	if($parent.children("#logo").width() + $parent.children("ul").width() + 30 > $parent.width() - $parent.children(".register").width()) {
+	if(logo+nav+30 > $parent.outerWidth() - $parent.children(".register").outerWidth()) {
+		$parent.find("ul").hide();
+		$parent.find("#logo .label").hide();
+		$parent.find(".menu").css("display", "inline-block");
+	} else if(logo+label+nav+30 > $parent.outerWidth() - $parent.children(".register").outerWidth()) {
+		$parent.find("ul").show();
 		$parent.find("#logo .label").hide();
 		$parent.find("#logo").css("margin-right", "10px");
-	} else {
+		$parent.find(".menu").hide();
+	} else  {
 		$parent.find("#logo .label").show();
 		$parent.find("#logo").css("margin-right", "30px");
+		$parent.find(".menu").hide();
 	}
 	$(".container").css("margin-top", $parent.outerHeight());
 	$(".button").each(function() {
@@ -16,7 +22,20 @@ function responsive() {
 	})
 }
 $(document).ready(function() {
+	logo = $(".action-bar #logo .logo").outerWidth();
+	label = $(".action-bar .label").outerWidth();
+	nav = $(".action-bar ul").outerWidth();
 	responsive();
+	$(".action-bar .menu").click(function() {
+		$(".slider").animate({
+			"left": "0px"
+		}, 500);
+	})
+	$(".slider a").click(function() {
+		$(".slider").animate({
+			"left": "-300px"
+		}, 500);
+	})
 })
 $(window).resize(function() {
 	responsive();
